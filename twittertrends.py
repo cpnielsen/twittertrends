62a1c6112c9218311d7e157e67fd267c63f2951b
Christian Palmhøj <christian@23company.com>
2012-12-05 15:24:46 +0100

    Updated structure, added TrendingTopics-listener

--- twittertrends.py
+++ twittertrends.py
@@ -1,67 +1,110 @@
 from tweepy import OAuthHandler, Stream
 from tweepy.streaming import StreamListener
 import json
-from streamlistener import StdOutListener
+import time
+import operator
+from random import random
 
-class TwitterTrends:
+CONSUMER_KEY="2XhwGwTJefx4aD2xdo4UoQ"
+CONSUMER_SECRET="yX5WYFz9LugOWyMgtkv4iWOvhBjGGZStmA61icFyi4"
 
-	def __init__(self):
-		#initiate the reuqired authentication
-		self.auth = OAuthHandler(consumer_key, consumer_secret)
-		self.auth.set_access_token(access_token, access_token_secret)
-
-		self.trendingtopics = []
-
-		self.l = StdOutListener(self)
-		self.stream = Stream(self.auth, self.l)
-		self.stream.filter(track=['*'])
-
-	def trendingtopics(self):
-		# Read data stream from streamListener and find trending topics
-		return "#itu, #sad2, #obama"
-
-	def misragries(self):
-		#Perform misra-gries algorithm
-		return self.trendingtopics
-
-	def distincttopics(self):
-		#use an algorithm to find distinct/rising topics
-		return "#itu, #obama, #sad2"
-
-	def newtweet(self, message, topic, time, user):
-		#callback from listener, create new tweet
-		tweet = Tweet(message, topic, time, user)
-		self.trendingtopics.append(tweet)
-
-		print "New tweet!"
+ACCESS_TOKEN="21019503-cKlNTqPehUAlyFR1FqcqjXPVK57xf3Jz4uEEk7pUz"
+ACCESS_TOKEN_SECRET="9gmuOaf9pf1nUV3Z87XDLL9Fwh47JTQonlinGinPRE"
 
 class Tweet:
-	def __init__(self, message, topic, time, user):
-		self.message = message
-		self.topic = topic
-		self.time = time
-		self.user = user
+    def __init__(self, message, topics, time, user):
+        self.message = message
+        self.topics = topics
+        self.time = time
+        self.user = user
 
-# class TwitterAuth:
-# 	consumer_key="2XhwGwTJefx4aD2xdo4UoQ"
-# 	consumer_secret="yX5WYFz9LugOWyMgtkv4iWOvhBjGGZStmA61icFyi4"
+class SimpleTweetWriter:
+    def on_tweet(self, tweet):
+        print u'%s: %s - %s' % (tweet.user, tweet.message, ', '.join(tweet.topics))
 
-# 	access_token="21019503-cKlNTqPehUAlyFR1FqcqjXPVK57xf3Jz4uEEk7pUz"
-# 	access_token_secret="9gmuOaf9pf1nUV3Z87XDLL9Fwh47JTQonlinGinPRE"
+class TrendingTopics:
+    def __init__(self, buckets):
+        self.topics = {}
+        self.buckets = buckets
+        self.count = 0
 
-# 	def __init__(self):
-# 		self.auth = OAuthHandler(consumer_key, consumer_secret)
-#     	self.auth.set_access_token(access_token, access_token_secret)
+    def on_tweet(self, tweet):
+        for tag in tweet.topics:
+            if tag in self.topics:
+                self.topics[tag] = self.topics[tag] + 1
+            else:
+                if len(self.topics) <= self.buckets:
+                    self.topics[tag] = 1
+                else:
+                    found = None
+                    for topic, value in self.topics.iteritems():
+                        if value == 0:
+                            found = topic
+                            break
+                    if found != None:
+                        del self.topics[found]
+                        self.topics[tag] = 1
+                    else:
+                        for topic, value in self.topics.iteritems():
+                            self.topics[topic] = value - 1
 
-# Go to http://dev.twitter.com and create an app. 
-# The consumer key and secret will be generated for you after
-consumer_key="2XhwGwTJefx4aD2xdo4UoQ"
-consumer_secret="yX5WYFz9LugOWyMgtkv4iWOvhBjGGZStmA61icFyi4"
+            self.count = self.count + 1
+            #if tag in self.topics:
+            #    print u"Tag '%s' has %d value" % (tag, self.topics[tag])
+        
+        if self.count > 1000:
+            self.count = 0
+            self.gettopics()
 
-# After the step above, you will be redirected to your app's page.
-# Create an access token under the the "Your access token" section
-access_token="21019503-cKlNTqPehUAlyFR1FqcqjXPVK57xf3Jz4uEEk7pUz"
-access_token_secret="9gmuOaf9pf1nUV3Z87XDLL9Fwh47JTQonlinGinPRE"
+    def gettopics(self, amount=10):
+        sorted_topics = sorted(self.topics.iteritems(), key=operator.itemgetter(1))
+        sorted_topics.reverse()
+
+        for x in xrange(amount):
+            print u'%d: %s' % (sorted_topics[x][1], sorted_topics[x][0])
+
+
+class TwitterTrends(StreamListener):
+    def __init__(self):
+        #initiate the reuqired authentication
+        auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+        self.stream = Stream(auth, self)
+
+        self.subscribers = []
+
+    def on_data(self, data):
+        for entry in data.split("\r\n"):
+            processed = json.loads(entry)
+
+            # Only include tweets that has a "topic", in this case one or more hashtags
+            if "entities" in processed:
+                if len(processed['entities']['hashtags']) > 0:
+                    tags = ["#" + t['text'] for t in processed['entities']['hashtags']]
+                    user = processed['user']['screen_name']
+                    text = processed['text']
+                    time = processed['created_at']
+
+                    tweet = Tweet(text, tags, time, user)
+
+                    for subscriber in self.subscribers:
+                        subscriber.on_tweet(tweet)
+
+
+    def on_error(self, error):
+        print u"Error occurred: %s" % error
+
+    def add_subscriber(self, subscriber):
+        self.subscribers.append(subscriber)
+
+    def start(self, topics=['*']):
+        self.stream.sample()
+
+    def stop(self):
+        self.stream.disconnect()
+
+
 
 if __name__ == '__main__':
     # l = StdOutListener()
@@ -70,3 +113,7 @@ if __name__ == '__main__':
     # stream = Stream(auth, l)
     # stream.filter(track=['*'])
     trends = TwitterTrends()
+    trendtopics = TrendingTopics(250)
+    trends.add_subscriber(trendtopics)
+    #trends.add_subscriber(SimpleTweetWriter())
+    trends.start()
